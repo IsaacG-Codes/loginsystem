5014bbfaf62c7a8e5a9f7a317f77639ae85eac0a
Isa-ac-G <Isaac.Gao@students.jmcss.org>
2024-09-27 17:45:34 -0500

    loginsystem
    loginsystem

---
+++ includes/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+<body>
+    <header>
+      <nav class="nav-header-main">
+        <a class="header-logo" href="index.php">
+          <img src="img/logo.png" alt="logo">
+        </a>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="#">Portfolio</a></li>
+      <li><a href="#">About me</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+      <div class="header-login">
+        <form action="includes/login.inc.php" method="POST">
+        <input type="text" name="mailuid" placeholder="Username/E-mail...">
+        <input type="password" name="pwd" placeholder="Password...">
+        <button type="submit" name="login-submit">Login</button>
+
+      </form>
+      <a href="signup.php">Signup</a>
+      <form action="includes/login.inc.php" method="POST">
+        <button type="submit" name="logout-submit">Logout</button>
+      </div>
+    </nav>
+  </header>
+</body>
+</html>
