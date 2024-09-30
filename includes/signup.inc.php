bc464ee34949cd5903f4b823cfa27bd4032170c1
IsaacG-Codes <Isaac.Gao@students.jmcss.org>
2024-09-30 08:31:09 -0500

    loginsystem2
    1:17:47

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -25,7 +25,7 @@
                 exit();
         }
         elseif($password !== $passwordRepeat){
-            header("Location: ../signup.php?error=passwordcheck&mail=".$email);
+            header("Location: ../signup.php?error=passwordcheck&uid=".$username."&mail=".$email);
             exit();
         }
         else {
@@ -38,8 +38,36 @@
             }
             else {
                 mysqli_stmt_bind_param($stmt, "s", $username);
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_store_result($stmt);
+                $resultCheck = mysqli_stmt_num_rows($stmt);
+                if ($resultCheck > 0){
+                    header("Location: ../signup.php?error=usertaken&&mail=".$email);
+                exit();
+                }
+                else {
+                    $sql = "INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES (?, ?, ?)";
+                    $stmt = mysqli_stmt_init($conn);
+                    if (!mysqli_stmt_prepare($stmt, $sql)) {
+                        header("Location: ../signup.php?error=sqlerror");
+                        exit();
+                    }
+                    else {
+                        $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
 
-                
+                        mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd);
+                        mysqli_stmt_execute($stmt);
+                        header("Location: ../signup.php?signup=success");
+                        exit();
+                    }
+                }
             }
         }
+        mysqli_stmt_close($stmt);
+        mysqli_close($conn);
+    }
+
+    else {
+        header("Location: ../signup.php");
+        exit();
     }
